8fbf61a549eac06f4d20a4f20a683f37fd80d356
isaacjeyakumarj <160233112+isaacjeyakumarj@users.noreply.github.com>
2024-08-22 16:14:14 +0530

    project_setup

--- elsevier_spotlight/index/views.py
+++ elsevier_spotlight/index/views.py
@@ -6,4 +6,15 @@ from django.http import HttpResponse
 def home(request):
     return render(request,'index.html')
     #return HttpResponse('this is homepage')
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def categories(request):
+    return render(request,'categories.html')
+
+def topblog(request):
+    return render(request,'topblog.html')
 
--- elsevier_spotlight/industry/urls.py
+++ elsevier_spotlight/industry/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
    
-    path('',views.industry),
+    path('',views.industry,name='industry'),
     
 ]
