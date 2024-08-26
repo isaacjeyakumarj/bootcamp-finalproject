7ff557f2708a04923472d4ca928ecb3f834834ad
isaacjeyakumarj <160233112+isaacjeyakumarj@users.noreply.github.com>
2024-08-26 15:00:54 +0530

    latest for loop and health css change

--- elsevier_spotlight/elsevier_spotlight/settings.py
+++ elsevier_spotlight/elsevier_spotlight/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / 'assets'
+
+MEDIA_URL = '/media/'
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- elsevier_spotlight/health/models.py
+++ elsevier_spotlight/health/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class healthblog(models.Model):
+    title=models.CharField(max_length=100,blank=False)
+    date=models.DateTimeField(auto_now_add=True)
+    categories=models.CharField(max_length=300,blank=False)
+    tags=models.CharField(max_length=300,blank=False)
+    text=models.CharField(max_length=300,blank=False)
+    image=models.ImageField(upload_to='healthblog/',blank=False)
+    image1=models.ImageField(upload_to='healthblog/',blank=False)
+    image2=models.ImageField(upload_to='healthblog/',blank=False)
+
+    def __str__(self):
+        return self.title
