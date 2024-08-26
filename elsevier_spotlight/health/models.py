524a0ddc36b6e5ac7a64a1610f3f1cc592335fe1
isaacjeyakumarj <160233112+isaacjeyakumarj@users.noreply.github.com>
2024-08-26 15:12:03 +0530

    add name in the health model

--- elsevier_spotlight/health/models.py
+++ elsevier_spotlight/health/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class healthblog(models.Model):
     title=models.CharField(max_length=100,blank=False)
+    name=models.CharField(max_length=100,blank=False,default="Default Value")
     date=models.DateTimeField(auto_now_add=True)
     categories=models.CharField(max_length=300,blank=False)
     tags=models.CharField(max_length=300,blank=False)
